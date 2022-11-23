f53a234d70e942be755a0cd3e37d9ce6f0c42ecd
melquis1420 <46160910+melquis1420@users.noreply.github.com>
2022-11-23 15:54:53 -0300

    FirstCommit

---
+++ app/src/main/java/com/example/apiuse/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.apiuse.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.apiuse.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
